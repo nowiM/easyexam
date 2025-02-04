e90b0605c2b1d1e8e9abef067c93b328d4e6c9d3
KimDoHyeon <ghkdtprhf5@naver.com>
2025-02-04 21:19:45 +0900

    fix: topiceData에 관련된 비동기 처리 함수 경로 수정

--- src/app/components/TopicePage/DataFetcher.js
+++ src/app/components/TopicePage/DataFetcher.js
@@ -1,20 +1,20 @@
 'use client'
 import { useSuspenseQuery } from '@tanstack/react-query'
-import topicPagefetchData from '@/app/utils/TopicPage/topicPagefetchData'
+import fetchTopiceData from '../../utils/shared/fetchTopiceData'
 import answerFetchData from '../../utils/TopicPage/answerFetchData'
 import Form from '../TopicePage/Form'
 
 const DataFetcher = ({ id }) => {
     const {data: topiceData} = useSuspenseQuery({
         queryKey: ['topiceData'],
-        queryFn: () => topicPagefetchData(`${process.env.NEXT_PUBLIC_API_URL}api/topices/${id}`)
+        queryFn: () => fetchTopiceData(`${process.env.NEXT_PUBLIC_API_URL}api/topices/${id}`)
     });
     
     const {data: answerData} = useSuspenseQuery({
         queryKey: ['answerData', id],
         queryFn: () => answerFetchData(id, `${process.env.NEXT_PUBLIC_API_URL}api/answers`)
     });
-    console.log(topiceData, answerData);
+
     return <Form topiceData={topiceData} answerData={answerData} />
 }
 
