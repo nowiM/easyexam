983be6d5a181e39d39ebe6c4149b51fcb514313d
KimDoHyeon <ghkdtprhf5@naver.com>
2025-02-06 16:30:50 +0900

    fix: 필요없는 주석 제거

--- src/app/utils/TopicPage/fetchAnswersData.js
+++ src/app/utils/TopicPage/fetchAnswersData.js
@@ -1,8 +1,6 @@
 const answerFetchData = async (id, url) => {
     const response = await fetch(url);
     const data = await response.json();
-    // console.log(data);
-    //data에 일치하는 data가 없는 경우 currentData에 어떻게 저장할 것인지
     const currentData = data.find(item => item.id === id) || { id, answers: [] };
 
     return currentData;
